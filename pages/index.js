4b63b400dea5bd018df2f2ad8d9e96af93e85728
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-10-10 16:06:44 +0700

    done

--- pages/index.js
+++ pages/index.js
@@ -53,7 +53,7 @@ export default function Home() {
               </li>
             </ul>
           </nav>
-          <div className="text-center p-10 py-10">
+          <div className="text-center p-2 py-2">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Iskandar Sholeh
             </h2>
