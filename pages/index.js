9e0e69e052b6342a10181a8fae180b870e6d210b
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-10-10 13:19:49 +0700

    new

--- pages/index.js
+++ pages/index.js
@@ -45,6 +45,8 @@ export default function Home() {
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
                   href="https://drive.google.com/file/d/1BC-1fIv1UKzUHln8phxM4l9uMf3EZqji/view?usp=sharing"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Resume
                 </a>
@@ -56,28 +58,24 @@ export default function Home() {
               Iskandar Sholeh
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-             Fullstack Developer | Associate Android developer
+              Fullstack Developer | Associate Android Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-            As the Fullstack developer. I have a strong understanding of the web development, from front-end development 
-            with HTML, CSS, and JavaScript to back-end development with PHP and MySQL. I'm also proficient in a variety of 
-            other technologies, including Mobile Development, and Cloud Computing.
+              As a Fullstack developer, I have a strong understanding of web development, 
+              from front-end development with HTML, CSS, and JavaScript to back-end development 
+              with PHP and MySQL. I'm also proficient in a variety of other technologies, 
+              including Mobile Development, and Cloud Computing.
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-            <a
-                href="https://github.com/iskandarsh/" target="_blank"
-                >
-              <AiFillGithub/>  </a>
-            <a
-                href="https://www.linkedin.com/in/iskandar-sholeh/" target="_blank"
-                >
-              <AiFillLinkedin />  </a>
-            <a
-                href="https://www.instagram.com/kconk.iskan/" target="_blank"
-                >
-              <AiFillInstagram /> </a>
-            
-            
+              <a href="https://github.com/iskandarsh/" target="_blank" rel="noopener noreferrer">
+                <AiFillGithub />
+              </a>
+              <a href="https://www.linkedin.com/in/iskandar-sholeh/" target="_blank" rel="noopener noreferrer">
+                <AiFillLinkedin />
+              </a>
+              <a href="https://www.instagram.com/kconk.iskan/" target="_blank" rel="noopener noreferrer">
+                <AiFillInstagram />
+              </a>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
               <Image src={iskandar} layout="fill" objectFit="cover" />
@@ -86,43 +84,26 @@ export default function Home() {
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
               Since the beginning of my journey as a freelance developer, I&apos;ve done remote work for
               <span className="text-teal-500"> agencies </span>
               consulted for <span className="text-teal-500">IT </span>
-              and collaborated with talanted people to create digital products
+              and collaborated with talented people to create digital products
               for both business and consumer use.
             </p>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including 
-              programming and teaching.
+              I offer a wide range of services, including programming and teaching.
             </p>
           </div>
           <div className="lg:flex gap-10">
-            {/* <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
-              </h3>
-              <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div> */}
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={code} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">
+              <h3 className="text-lg font-medium pt-8 pb-2">
                 Code your dream project
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let&apos;s make it a
-                reality.
+                Do you have an idea for your next great website? Let&apos;s make it a reality.
               </p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
               <p className="text-gray-800 py-1">Text Editor</p>
@@ -132,10 +113,9 @@ export default function Home() {
             </div>
             <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
               <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
+              <h3 className="text-lg font-medium pt-8 pb-2">Consulting</h3>
               <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
+                Are you interested in feedback for your current project? I can give you tips and tricks to level it up.
               </p>
               <h4 className="py-4 text-teal-600">Tools I Use</h4>
               <p className="text-gray-800 py-1">Meet</p>
@@ -147,73 +127,26 @@ export default function Home() {
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">My Recent Works</h3>
-            {/* <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance developer, I&apos;ve done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">IT </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including 
-              programming and teaching.
-            </p> */}
+            <h3 className="text-3xl py-1 dark:text-white">My Recent Works</h3>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web1}
-              />
+            <div className="basis-1/3 flex-1">
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web1} />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web2}
-              />
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web2} />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web3} />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web4} />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web5} />
             </div>
             <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
+              <Image className="rounded-lg object-cover" width={"100%"} height={"100%"} layout="responsive" src={web6} />
             </div>
           </div>
         </section>
