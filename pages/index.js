bb906a819dc2b17d7d5a7e79e2f3025342fbcf48
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-10-10 16:00:37 +0700

    sip

--- pages/index.js
+++ pages/index.js
@@ -54,12 +54,12 @@ export default function Home() {
             </ul>
           </nav>
           <div className="text-center p-10 py-10">
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={iskandar} layout="fill" objectFit="cover" />
-            </div>
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
               Iskandar Sholeh
             </h2>
+            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
+              <Image src={iskandar} layout="fill" objectFit="cover" />
+            </div>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
               Fullstack Developer | Associate Android Developer
             </h3>
