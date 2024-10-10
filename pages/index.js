22ee68b04090161d879c3f4ad28b10aa70f2a4c1
Iskandar Sholeh <iskandar.sholeh22@gmail.com>
2024-10-10 15:58:24 +0700

    baru

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import {
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
-import iskandar from "../public/profile-iskandar.png";
+import iskandar from "../public/profile-iskandar2.png";
 import code from "../public/code.png";
 import design from "../public/design.png";
 import consulting from "../public/consulting.png";
